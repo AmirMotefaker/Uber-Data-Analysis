919efe58d8c9f68c2aa97c262fc358bf91e7ac82
Amir Motefaker <amotef@gmail.com>
2022-12-27 16:15:09 +0330

    Plot a heatmap by Collect data by month and day

--- Uber-Data-Analysis.R
+++ Uber-Data-Analysis.R
@@ -271,3 +271,9 @@ month_day_data
 #   10 Apr   10    20041
 # # … with 173 more rows
 # # ℹ Use `print(n = ...)` to see more rows
+
+
+# Plot a heatmap by Collect data by month and day
+ggplot(month_day_data, aes(day, month, fill = Trips)) + 
+  geom_tile(color = "white") + 
+  ggtitle("Heat Map by Month and Day")

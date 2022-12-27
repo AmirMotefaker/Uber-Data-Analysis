07b4978b5a922a3be161b233878d7b6fcefd2237
Amir Motefaker <amotef@gmail.com>
2022-12-27 16:15:57 +0330

    Plot a heatmap by day of the week and month

--- Uber-Data-Analysis.R
+++ Uber-Data-Analysis.R
@@ -277,3 +277,9 @@ month_day_data
 ggplot(month_day_data, aes(day, month, fill = Trips)) + 
   geom_tile(color = "white") + 
   ggtitle("Heat Map by Month and Day")
+
+
+# Plot a heatmap by day of the week and month
+ggplot(day_month_data, aes(dayofweek, month, fill = Trips)) + 
+  geom_tile(color = "white") + 
+  ggtitle("Heat Map by Month and Day")

37bfbea52c0ae06ea68e84435962ee1894188b21
Amir Motefaker <amotef@gmail.com>
2022-12-28 12:17:52 +0330

    Creating vector of colors for the plots

--- Uber Data Analysis (JupyterNotebook).r
+++ Uber Data Analysis (JupyterNotebook).r
@@ -6,3 +6,7 @@ library(dplyr)
 library(tidyr)
 library(DT)
 library(scales)
+
+# Creating vector of colors for the plots
+colors = c("#CC1011", "#665555", "#05a399", "#cfcaca", "#f5e840", "#0683c9", "#e075b0")
+colors

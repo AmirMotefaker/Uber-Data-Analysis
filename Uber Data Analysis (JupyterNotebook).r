ae1344be1cacc03d09913cfa12eb4fde4a38fabe
Amir Motefaker <amotef@gmail.com>
2022-12-28 12:39:40 +0330

    Read the data for each month separately

--- Uber Data Analysis (JupyterNotebook).r
+++ Uber Data Analysis (JupyterNotebook).r
@@ -10,3 +10,11 @@ library(scales)
 # Creating vector of colors for the plots
 colors = c("#CC1011", "#665555", "#05a399", "#cfcaca", "#f5e840", "#0683c9", "#e075b0")
 colors
+
+# Read the data for each month separately 
+apr <- read.csv("D:/AMIR/Programming/Data Science/R Projects/Uber Data Analysis/Dataset/uber-raw-data-apr14.csv")
+may <- read.csv("D:/AMIR/Programming/Data Science/R Projects/Uber Data Analysis/Dataset/uber-raw-data-may14.csv")
+june <- read.csv("D:/AMIR/Programming/Data Science/R Projects/Uber Data Analysis/Dataset/uber-raw-data-jun14.csv")
+july <- read.csv("D:/AMIR/Programming/Data Science/R Projects/Uber Data Analysis/Dataset/uber-raw-data-jul14.csv")
+aug <- read.csv("D:/AMIR/Programming/Data Science/R Projects/Uber Data Analysis/Dataset/uber-raw-data-aug14.csv")
+sept <- read.csv("D:/AMIR/Programming/Data Science/R Projects/Uber Data Analysis/Dataset/uber-raw-data-sep14.csv")
